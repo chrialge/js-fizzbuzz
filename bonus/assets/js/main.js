ea63db70c7642ba1dbfa14659b5e62920c158027
chrialge <chrialge@gmail.com>
2024-02-21 16:13:55 +0100

    add half bonus 1

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -3,6 +3,8 @@ console.log("hello");
 // dichiaro la variabile dove includo container_square(controllino con console.log)
 const containerSquare = document.getElementById("container_square");
 console.log(containerSquare)
+const square = document.createElement('li')
+square.classList.add("box");
 // crea tre variabili con il contenuto dei tre riquadri non numerati
 const fizz = "fizz";
 const buzz = "buzz";
@@ -18,7 +20,10 @@ for (let i = 1; i <= 100; i++){
     if (i% 3 === 0 && i% 5 ===0) {
         // SE e divisibili per 3 e per 5
         console.log(fizzBuzz); 
-        containerSquare.insertAdjacentHTML('beforeend', `<li class="box fizzbuzz_bg">${fizzBuzz}</li>`)
+        square.classList.add("fizzbuzz_bg")
+        square.append(`${fizzBuzz}`)
+        containerSquare.append(square)
+        console.log(square)
     } else if (i%5 ===0){
         // ALTRIMENTI SE e divisibile per 5
        console.log(buzz); 
