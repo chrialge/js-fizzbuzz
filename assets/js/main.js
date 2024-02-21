94608239d87f8f154ac7a65df17fcc2b67405e60
chrialge <chrialge@gmail.com>
2024-02-21 14:45:57 +0100

    create structure for with if/elseif/else

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
 console.log("hello");
+
+// dichiaro la variabile dove includo container_square(controllino con console.log)
+const containerSquare = document.getElementById("container_square");
+console.log(containerSquare)
+let fizz = "fizz";
+let buzz = "buzz";
+
+for (let i = 1; i <= 100; i++){
+    console.log(i)
+    
+    if (i% 3 === 0) {
+        console.log(fizz);
+    } else if (i%5 ===0){
+       console.log(buzz); 
+    }else{
+        console.log(i)
+    }
+
+    containerSquare.insertAdjacentHTML('beforeend', `<li>${i}</li>`)
+}
