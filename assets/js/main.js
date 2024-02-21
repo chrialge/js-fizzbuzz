d2d84473bbbff7ddef77cfc832e06c7deee51051
chrialge <chrialge@gmail.com>
2024-02-21 15:27:15 +0100

    add fizzbuzz and condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,9 @@ console.log("hello");
 // dichiaro la variabile dove includo container_square(controllino con console.log)
 const containerSquare = document.getElementById("container_square");
 console.log(containerSquare)
-let fizz = "fizz";
-let buzz = "buzz";
+const fizz = "fizz";
+const buzz = "buzz";
+const fizzBuzz = "fizzbuzz"
 
 // crea un ciclo per creare una serie di riquadri
 for (let i = 1; i <= 100; i++){
@@ -13,12 +14,15 @@ for (let i = 1; i <= 100; i++){
     /* 
     crea delle condizioni in cui se e divisibile per 3(fizz) per 5(buzz) il resto numeri a pregressione
     */
-    if (i% 3 === 0) {
-        console.log(fizz);
-        containerSquare.insertAdjacentHTML('beforeend', `<li class="box fizz_bg">${fizz}</li>`)
+    if (i% 3 === 0 && i% 5 ===0) {
+        console.log(fizzBuzz); 
+        containerSquare.insertAdjacentHTML('beforeend', `<li class="box fizzbuzz_bg">${fizzBuzz}</li>`)
     } else if (i%5 ===0){
        console.log(buzz); 
        containerSquare.insertAdjacentHTML('beforeend', `<li class="box buzz_bg">${buzz}</li>`)
+    }else if(i%3 ===0){
+       console.log(fizz);
+       containerSquare.insertAdjacentHTML('beforeend', `<li class="box fizz_bg">${fizz}</li>`)
     }else{
         console.log(i)
         containerSquare.insertAdjacentHTML('beforeend', `<li class="box bg_secondary">${i}</li>`)
